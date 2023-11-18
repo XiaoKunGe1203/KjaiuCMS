8f5d278fe12c8605884161249c4650b0663d8676
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-11-18 22:55:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/*
+nginx伪静态
+location / {
+	if (!-e $request_filename){
+		rewrite  ^(.*)$  /index.php?s=$1  last;   break;
+	}
+}
+*/
 include 'config.php';
 
 $conn = new mysqli($sqlinfo['host'], $sqlinfo['username'], $sqlinfo['password'], $sqlinfo['dbname']);
@@ -17,12 +25,15 @@ $sql = "SELECT * FROM `HomePages` WHERE `s` LIKE '". $pages ."'";
 $result = $conn->query($sql);
 $conn->close();
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-           require $row['www'] .'/'. $row['filename'];
-    }
+    $row = $result->fetch_assoc();  
+    require $row['www'] .'/'. $row['filename'];
 } else {
       // 如果文件不存在，输出错误消息
       $error = '文件不存在';
       $ecode = '404';
       require 'error.php';
 } 
+/*
+$sql = "UPDATE `dashboard_users` SET `authentication` = '". $authentication ."' WHERE `dashboard_users`.`id` = 0;
+";
+phpinfo(); */
