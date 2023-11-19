961df5d7d308f89f219242f5fc944ad0b54b4362
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-11-19 15:02:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 //SQL变量
-$sqlinfo['host'] = 'localhost';
-$sqlinfo['username'] = ' ';
-$sqlinfo['password'] = ' ';
-$sqlinfo['dbname'] = ' ';
+$sqlinfo['host'] = 'localhost'; //Mysql地址
+$sqlinfo['username'] = 'app'; //Mysql账户
+$sqlinfo['password'] = 'app'; //Mysql密码
+$sqlinfo['dbname'] = 'app'; //Mysql数据库名
 //SITE变量
-$siteinfo['web_url'] = 'http://www.kjaiu.link';
-$siteinfo['web_name'] = '幻心互联';
-$siteinfo['company_email'] = '没有';
+$siteinfo['web_url'] = 'http://www.example.com'; //URL地址
+$siteinfo['web_name'] = 'example'; //网站名称
+$siteinfo['company_email'] = 'example@example.com'; //联系邮箱
+?>
